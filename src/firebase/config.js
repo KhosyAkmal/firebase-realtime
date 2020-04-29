adb848d6ed7dff4f32605ecc9ace007a8bc7bcf3
KhosyAkmal <akmalkhosy@gmail.com>
2020-04-30 03:49:47 +0700

    add config.js

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDl8iWHHl-Y1YyRTF4xkOUxiK8JRsEYZPY",
+    authDomain: "fir-login-27e06.firebaseapp.com",
+    databaseURL: "https://fir-login-27e06.firebaseio.com",
+    projectId: "fir-login-27e06",
+    storageBucket: "fir-login-27e06.appspot.com",
+    messagingSenderId: "579967989351",
+    appId: "1:579967989351:web:c5f071843f15ec51dd46c2",
+    measurementId: "G-58MFY2R8Z2"
+  };
+
+  export default firebaseConfig;
